663690ea8e59cec633034582200643cef56c1f99
lucasrp <lcsrpaiva@gmail.com>
2021-06-04 01:01:41 +0200

    Add Navigation to News Fragment

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -6,9 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.main_container, NewsListFragment())
-            .commit()
     }
 }
